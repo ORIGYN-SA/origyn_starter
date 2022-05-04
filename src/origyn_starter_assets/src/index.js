9c691781852dd703caf05f95f48ff58406f197fa
Fer Martz <fernandomvc@gmail.com>
2022-05-04 15:16:45 -0500

    Initial commit.

---
+++ src/origyn_starter_assets/src/index.js
@@ -0,0 +1,19 @@
+import { origyn_starter } from "../../declarations/origyn_starter";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await origyn_starter.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
